0b7d0afa39d92e2fe5bfc9ca8f398981cd303c11
Manav Misra <manavm@visionwebsoft.com>
2020-10-26 17:19:52 -0500

    :recycle: Move callback into closure pattern
    Avoiding code clutter/repetition.

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,20 @@
 import client from "./client";
 
+const callback = (func) => (err, res) => {
+  if (err) {
+    func(err);
+  } else {
+    func(null, res);
+  }
+};
+
 export default {
   add(title, author, cb) {
     client.insert(
       { table: "books", records: [{ title, author }] },
-      (err, res) => {
-        if (err) {
-          cb(err);
+      callback(cb)
+    );
+  },
         } else {
           cb(null, res);
         }
